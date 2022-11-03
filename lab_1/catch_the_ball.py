81365d64396101f79051610f6086687ccc2b6abd
Safonov Roman <rs-pilot@yandex.ru>
2022-11-03 15:09:02 +0500

    Now there are two balls on the screen, which have random radiuses and random velocities. Try to catch them all

--- lab_1/catch_the_ball.py
+++ lab_1/catch_the_ball.py
@@ -3,7 +3,7 @@ from pygame.draw import *
 from random import randint
 pygame.init()
 
-FPS = 1
+FPS = 120
 screen = pygame.display.set_mode((1200, 900))
 
 RED = (255, 0, 0)
@@ -17,33 +17,36 @@ COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
 
 def new_ball_1():
     '''
-    Рисует первый шарик.
+    Рисует первый шарик в каджый момент времени.
     х1 -- координата центра шара по Ox, случайное целое число в диапазоне (100, 1100)
     y1 -- координата центра шара по Oy, случайное целое число в диапазоне (100, 900)
     r1 -- радиус шара, случайное целое число в диапазоне (10, 100)
     color -- цвет шара, выбирается случайно из COLORS
     '''
-    global x1, y1, r1
-    x1 = randint(100, 1100)
-    y1 = randint(100, 900)
-    r1 = randint(10, 100)
-    color = COLORS[randint(0, 5)]
-    circle(screen, color, (x1, y1), r1)
+    global x1, y1, r1, vx1, vy1
+    if ((y1 + r1 >= 900) or (y1 - r1 <= 0)):
+        vy1 *= -1
+    if ((x1 + r1 >= 1200) or (x1 - r1) <= 0):
+        vx1 *= -1
+    x1 += vx1
+    y1 += vy1
+    circle(screen, BLUE, (x1, y1), r1)
 
 def new_ball_2():
     '''
-    Рисует второй шарик.
+    Рисует второй шарик в каждый момент времени.
     х2 -- координата центра шара по Ox, случайное целое число в диапазоне (100, 1100)
     y2 -- координата центра шара по Oy, случайное целое число в диапазоне (100, 900)
     r2 -- радиус шара, случайное целое число в диапазоне (10, 100)
-    color -- цвет шара, выбирается случайно из COLORS
     '''
-    global x2, y2, r2
-    x2 = randint(100, 1100)
-    y2 = randint(100, 900)
-    r2 = randint(10, 100)
-    color = COLORS[randint(0, 5)]
-    circle(screen, color, (x2, y2), r2)
+    global x2, y2, r2, vx2, vy2
+    if ((y2 + r2 >= 900) or (y2 - r2 <= 0)):
+        vy2 *= -1
+    if ((x2 + r2 >= 1200) or (x2 - r2) <= 0):
+        vx2 *= -1
+    x2 += vx2
+    y2 += vy2
+    circle(screen, RED, (x2, y2), r2)
 
 def click(event):
     '''
@@ -73,6 +76,21 @@ pygame.display.update()
 clock = pygame.time.Clock()
 finished = False
 counter = 0
+
+# Начальные координаты шариков
+x1 = randint(100, 1100)
+y1 = randint(100, 800)
+r1 = randint(10, 100)
+
+x2 = randint(100, 1100)
+y2 = randint(100, 800)
+r2 = randint(10, 100)
+
+#Скорости шариков по Ox и Oy в условных единицах
+vx1 = randint(-5, 5)
+vy1 = randint(-5, 5)
+vx2 = randint(-5, 5)
+vy2 = randint(-5, 5)
 
 while not finished:
     clock.tick(FPS)

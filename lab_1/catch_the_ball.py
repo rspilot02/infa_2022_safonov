ed77dcac58e721fca2f485b9f92be9edd9d3d7a7
Safonov Roman <rs-pilot@yandex.ru>
2022-11-03 16:12:53 +0500

    New update! What's new: 1) Now there are two figures to hit: the adequate ball and the CRAZY square 2) When you hit the ball, you achieve one point and there is written 'Хорош! +1' 3) When you hit the square, you achieve three points and there is written 'МЕГАХОРОШ! +3' 4) The motion of the ball is easy to predict, but the motion of the square is almost unpredictable! Try yourself in that wonderful game

--- lab_1/catch_the_ball.py
+++ lab_1/catch_the_ball.py
@@ -18,10 +18,10 @@ COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
 def new_ball_1():
     '''
     Рисует первый шарик в каджый момент времени.
-    х1 -- координата центра шара по Ox, случайное целое число в диапазоне (100, 1100)
-    y1 -- координата центра шара по Oy, случайное целое число в диапазоне (100, 900)
-    r1 -- радиус шара, случайное целое число в диапазоне (10, 100)
-    color -- цвет шара, выбирается случайно из COLORS
+    х1 -- координата центра шара по Ox
+    y1 -- координата центра шара по Oy
+    r1 -- радиус шара
+    vx1, vy1 -- составляющие скорости шара по Ox и по Oy
     '''
     global x1, y1, r1, vx1, vy1
     if ((y1 + r1 >= 900) or (y1 - r1 <= 0)):
@@ -32,65 +32,68 @@ def new_ball_1():
     y1 += vy1
     circle(screen, BLUE, (x1, y1), r1)
 
-def new_ball_2():
+def new_rec_2():
     '''
-    Рисует второй шарик в каждый момент времени.
-    х2 -- координата центра шара по Ox, случайное целое число в диапазоне (100, 1100)
-    y2 -- координата центра шара по Oy, случайное целое число в диапазоне (100, 900)
-    r2 -- радиус шара, случайное целое число в диапазоне (10, 100)
+    Рисует квадрат в каждый момент времени.
+    х2 -- координата левой верхней вершины по Ox
+    y2 -- координата левой верхней вершины по Oy
+    vx2, vy2 -- составляющие скорости квадрата по Ox и по Oy
     '''
-    global x2, y2, r2, vx2, vy2
-    if ((y2 + r2 >= 900) or (y2 - r2 <= 0)):
-        vy2 *= -1
-    if ((x2 + r2 >= 1200) or (x2 - r2) <= 0):
-        vx2 *= -1
+    global x2, y2, vx2, vy2
+    if ((y2 + 100 >= 900) or (y2 <= 0)):
+        vy2 = -abs(vr[randint(0, 11)]) * vy2 / abs(vy2)
+        vx2 = vr[randint(0, 11)]
+        if (y2 <= 0):
+            y2 = -y2
+        elif (y2 + 100 >= 900):
+            y2 = y2 - 2*abs(y2 - 800)
+    if ((x2 + 100 >= 1200) or (x2 <= 0)):
+        if (x2 <= 0):
+            x2 = -x2
+        elif (x2 + 100 >= 1200):
+            x2 = x2 - 2*abs(x2 - 1100)
+        vy2 = vr[randint(0, 11)]
+        vx2 = -abs(vr[randint(0, 11)]) * vx2 / abs(vx2)
     x2 += vx2
     y2 += vy2
-    circle(screen, RED, (x2, y2), r2)
+    rect(screen, RED, (x2, y2, 100, 100))
 
-def click(event):
+def hit_ball(x_c, y_c, x1, y1, r1):
     '''
-    Возвращает данные о шариках
-    x1 -- координата центра первого шарика по Ox
-    y1 -- координата центра первого шарика по Oy
-    r1 -- радуис первого шарика
-    x2 -- координата центра второго шарика по Ox
-    y2 -- координата центра второго шарика по Oy
-    r2 -- радуис второго шарика
-    '''
-    return x1, y1, r1, x2, y2, r2
-
-def hit(x_c, y_c, x1, y1, r1, x2, y2, r2):
-    '''
-    Определяет, попал ли игрок хотя бы по одному шарику.
+    Определяет, попал ли игрок по шарику.
     x_c, y_c -- координаты клика по Ox, Oy
-    x1, y1 -- координаты центра первого шарика по Ox, Oy
+    x1, y1 -- координаты центра шарика по Ox, Oy
     r1 -- радиус первого шарика
-    x2, y2 -- координаты центра второго шарика по Ox, Oy
-    r2 -- радиус второго шарика
     '''
-    return ((((x1 - x_c) ** 2 + (y1 - y_c) ** 2) <= r1 ** 2) or
-            (((x2 - x_c) ** 2 + (y2 - y_c) ** 2) <= r2 ** 2))
+    return (((x1 - x_c) ** 2 + (y1 - y_c) ** 2) <= r1 ** 2)
+
+def hit_rect(x_c, y_c, x2, y2):
+    '''
+    Определяет, попал ли игрок по квадрату.
+    x_c, y_c -- координаты клика по Ox, Oy
+    x2, y2 -- координаты левой верхней вершины квадрата по Ox, Oy
+    '''
+    return (x2 - x_c <= 100) and (y2 - y_c <= 100)
 
 pygame.display.update()
 clock = pygame.time.Clock()
 finished = False
 counter = 0
 
-# Начальные координаты шариков
+# Начальные координаты мишеней
 x1 = randint(100, 1100)
 y1 = randint(100, 800)
-r1 = randint(10, 100)
+r1 = 50
 
-x2 = randint(100, 1100)
-y2 = randint(100, 800)
-r2 = randint(10, 100)
+x2 = randint(0, 1100)
+y2 = randint(0, 800)
 
-#Скорости шариков по Ox и Oy в условных единицах
-vx1 = randint(-5, 5)
-vy1 = randint(-5, 5)
-vx2 = randint(-5, 5)
-vy2 = randint(-5, 5)
+# Скорости мишеней по Ox и Oy в условных единицах
+vx1 = 3
+vy1 = 3
+vr = [-10, -9, -8, -7, -6, -5, 5, 6, 7, 8, 9, 10]
+vx2 = vr[randint(0, 11)]
+vy2 = vr[randint(0, 11)]
 
 while not finished:
     clock.tick(FPS)
@@ -98,12 +101,14 @@ while not finished:
         if event.type == pygame.QUIT:
             finished = True
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            if hit(event.pos[0], event.pos[1], click(event)[0], click(event)[1],
-                   click(event)[2], click(event)[3], click(event)[4], click(event)[5]):
-                print("Попал!")
+            if hit_ball(event.pos[0], event.pos[1], x1, y1, r1):
+                print("Хорош! +1")
                 counter += 1
+            if hit_rect(event.pos[0], event.pos[1], x2, y2):
+                print("МЕГАХОРОШ! +3")
+                counter += 3
     new_ball_1()
-    new_ball_2()
+    new_rec_2()
     pygame.display.update()
     screen.fill(BLACK)
 
